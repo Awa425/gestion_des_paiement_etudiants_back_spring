ef632b414bba46f620a1692d4199f3113ebe2eb7
Awa425 <diopawa425@gmail.com>
2024-05-08 11:13:32 +0000

    first commit

---
+++ src/main/java/com/example/paiment/PaimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.paiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/paiment/PaimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
